a30c47d8ee573ec3554106df1967d7575088186d
syumai <syumai@gmail.com>
2022-09-04 01:40:37 +0900

    split store

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -4,19 +4,16 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/rs/cors"
 	"log"
 	"net/http"
-	"sort"
-	"sync"
-
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 
 	"github.com/bufbuild/connect-go"
 	"github.com/google/uuid"
+	"github.com/rs/cors"
 	v1 "github.com/syumai/emotter/api/gen/emotter/v1"
 	"github.com/syumai/emotter/api/gen/emotter/v1/emotterv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -25,35 +22,6 @@ var port = flag.Int("port", 8080, "e.g. 8080")
 type EmotterServer struct{}
 
 var _ emotterv1connect.EmotterServiceHandler = &EmotterServer{}
-
-var store sync.Map
-
-func storePost(post *v1.Post) {
-	store.Store(post.Id, post)
-}
-
-func loadPost(id string) (post *v1.Post, ok bool) {
-	v, ok := store.Load(id)
-	if !ok {
-		return nil, false
-	}
-	return v.(*v1.Post), true
-}
-
-func loadAllPosts(asc bool) []*v1.Post {
-	var posts []*v1.Post
-	store.Range(func(key, value any) bool {
-		posts = append(posts, value.(*v1.Post))
-		return true
-	})
-	sort.Slice(posts, func(i, j int) bool {
-		if !asc {
-			i, j = j, i
-		}
-		return posts[i].CreateTime.AsTime().Before(posts[j].CreateTime.AsTime())
-	})
-	return posts
-}
 
 func (e EmotterServer) CreatePost(
 	ctx context.Context,
---
+++ api/cmd/server/store.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sort"
+	"sync"
+
+	v1 "github.com/syumai/emotter/api/gen/emotter/v1"
+)
+
+var store sync.Map
+
+func storePost(post *v1.Post) {
+	store.Store(post.Id, post)
+}
+
+func loadPost(id string) (post *v1.Post, ok bool) {
+	v, ok := store.Load(id)
+	if !ok {
+		return nil, false
+	}
+	return v.(*v1.Post), true
+}
+
+func loadAllPosts(asc bool) []*v1.Post {
+	var posts []*v1.Post
+	store.Range(func(key, value any) bool {
+		posts = append(posts, value.(*v1.Post))
+		return true
+	})
+	sort.Slice(posts, func(i, j int) bool {
+		if !asc {
+			i, j = j, i
+		}
+		return posts[i].CreateTime.AsTime().Before(posts[j].CreateTime.AsTime())
+	})
+	return posts
+}
