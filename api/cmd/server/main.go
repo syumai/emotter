b7a8c9bf2430162c92a22a1d0f6f3e335e24b97e
syumai <syumai@gmail.com>
2022-09-04 11:57:01 +0900

    gracefully shutdown API server

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -7,6 +7,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/bufbuild/connect-go"
@@ -64,16 +67,29 @@ func main() {
 	mux := http.NewServeMux()
 	path, handler := emotterv1connect.NewEmotterServiceHandler(srv)
 	mux.Handle(path, handler)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	addr := fmt.Sprintf(":%d", *port)
 	s := http.Server{
-		Addr:              fmt.Sprintf(":%d", *port),
+		Addr:              addr,
 		Handler:           cors.Default().Handler(h2c.NewHandler(mux, &http2.Server{})),
 		ReadHeaderTimeout: time.Second,
 		ReadTimeout:       5 * time.Minute,
 		WriteTimeout:      5 * time.Minute,
 		MaxHeaderBytes:    8 * 1024, // 8KiB
 	}
-	err := s.ListenAndServe()
-	if err != nil {
+
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		s.Shutdown(ctx)
+	}()
+
+	fmt.Printf("listening server on %s\n", addr)
+	if err := s.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
 }
