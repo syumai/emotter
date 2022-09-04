49201b34dde1c5d447662846458e6ddeb2199e62
syumai <syumai@gmail.com>
2022-09-04 11:11:19 +0900

    add GitHub icon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,10 +14,25 @@ const App: FC = () => (
   <div className="app">
     <QueryClientProvider client={queryClient}>
       <EmotterServiceClientProvider transport={transport}>
-        <section className="hero is-info">
-          <div className="hero-body">
-            <p className="title">Emotter</p>
-            <p className="subtitle">post and share your emoji!</p>
+        <section className="hero is-info is-small">
+          <div className="hero-body columns">
+            <div className="column">
+              <p className="title">Emotter</p>
+              <p className="subtitle">post and share your emoji!</p>
+            </div>
+            <div
+              className="column is-one-quarter"
+              style={{ textAlign: "right" }}
+            >
+              <a className="button is-info is-inverted">
+                <a
+                  className="icon is-large"
+                  href="https://github.com/syumai/emotter"
+                >
+                  <i className="fab fa-github fa-2x"></i>
+                </a>
+              </a>
+            </div>
           </div>
         </section>
         <section className="section">
